d86de484bd046e01ffeed34c7c30b099752683f0
mark-solo <mark.an.impostor@gmail.com>
2019-11-05 20:50:23 +0300

    Set up testing environment

--- src/main/java/stc/App.java
+++ src/main/java/stc/App.java
@@ -1,9 +1,16 @@
 package stc;
 
-public class App 
+import java.util.HashSet;
+import java.util.Set;
+
+public class App
 {
     public static void main( String[] args )
     {
+        cleanup("Practically, anything", new HashSet<String>(), new HashSet<String>());
+    }
 
+    static void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) {
+        System.out.println("Hello, World!");
     }
 }
--- src/test/java/stc/AppTest.java
+++ src/test/java/stc/AppTest.java
@@ -1,10 +1,34 @@
 package stc;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashSet;
 
 public class AppTest 
 {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testHelloWorld() {
+        App.cleanup("Practically, anything", new HashSet<String>(), new HashSet<String>());
+        assertEquals("Hello, World!\r\n", outContent.toString()); // don't forget about windows line ending!
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOut);
+    }
 }
