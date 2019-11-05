8cbb3147cecf2115e0842a4ead616e0c89b05103
mark-solo <mark.an.impostor@gmail.com>
2019-11-05 22:58:55 +0300

    Wrote and tested (should be in reverse order) function checking whether object has all required fields, otherwise throws IllegalArgumentException

--- src/main/java/stc/App.java
+++ src/main/java/stc/App.java
@@ -1,16 +1,39 @@
 package stc;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Set;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         cleanup("Practically, anything", new HashSet<String>(), new HashSet<String>());
     }
 
-    static void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) {
+    /**
+     * - если в списках fieldsToCleanup или fieldsToOutput содержится поле, которого нет в объекте, падать с IllegalArgumentException, оставив объект неизменным
+     * TODO - изменять значения в полях по списку fieldsToCleanup
+     * TODO  - изменять значения полей примитивных типов на значения по умолчанию
+     * TODO  - изменять на null значения полей других типов
+     * TODO - выводить значения в полях по списку fieldsToOutput
+     * TODO  - выводить String.valueOf для полей примитивных типов
+     * TODO  - выводить toString для полей других типов
+     * TODO - если объект является реализацией интерфейса Map, то проделать аналогичные операции - для списка fieldsToCleanup удалить ключи из мапы, для fieldsToOutput вывести в консоль значения, хранящиеся в мапе.
+     */
+    static void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) throws IllegalArgumentException {
+        checkIfObjectHasAllFields(object, fieldsToCleanup);
+        checkIfObjectHasAllFields(object, fieldsToOutput);
+
         System.out.println("Hello, World!");
+    }
+
+    private static void checkIfObjectHasAllFields(Object object, Set<String> fieldSet) throws IllegalArgumentException {
+        Class clazz = object.getClass();
+
+        try {
+            for (String potentialFieldName : fieldSet)
+                clazz.getDeclaredField(potentialFieldName);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException(e.toString());
+        }
     }
 }
--- src/test/java/stc/AppTest.java
+++ src/test/java/stc/AppTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.HashSet;
+import java.util.Set;
 
 public class AppTest 
 {
@@ -22,9 +23,23 @@ public class AppTest
     }
 
     @Test
-    public void testHelloWorld() {
+    public void testOutput() {
         App.cleanup("Practically, anything", new HashSet<String>(), new HashSet<String>());
-        assertEquals("Hello, World!\r\n", outContent.toString()); // don't forget about windows line ending!
+        assertEquals("Hello, World!\r\n", outContent.toString()); // don't forget about windows line endings!
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwExceptionOnNonexistentCleanupField() throws IllegalArgumentException {
+        Set<String> hCleanup = new HashSet<>();
+        hCleanup.add("width");
+        App.cleanup("String, for example", hCleanup, new HashSet<String>());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwExceptionOnNonexistentOutputField() throws IllegalArgumentException {
+        Set<String> hOutput = new HashSet<>();
+        hOutput.add("height");
+        App.cleanup("String, for example", new HashSet<String>(), hOutput);
     }
 
     @After
