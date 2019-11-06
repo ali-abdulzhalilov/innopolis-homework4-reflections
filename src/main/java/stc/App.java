00ef45087c24f913fb8f083c2b58b2d2dd7f5d6a
mark-solo <mark.an.impostor@gmail.com>
2019-11-06 10:54:00 +0300

    Tested, wrote and tested again function for cleaning up fields mentioned in set fieldsToCleanup

--- src/main/java/stc/App.java
+++ src/main/java/stc/App.java
@@ -1,27 +1,64 @@
 package stc;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
-        cleanup("Practically, anything", new HashSet<String>(), new HashSet<String>());
+        class Node {
+            private String key;
+            private int value;
+            private Node next;
+
+            private Node(String key, int value, Node next) {
+                this.key = key;
+                this.value = value;
+                this.next = next;
+            }
+
+            public String getKey() {
+                return this.key;
+            }
+
+            public int getValue() {
+                return this.value;
+            }
+        }
+
+        String nodeKey = "some_key";
+        int nodeValue = 42;
+        Node nextNode = new Node("irrelevant_key", 427, null);
+        Node testSubject = new Node(nodeKey, nodeValue, nextNode);
+        Set<String> hCleanup = new HashSet<>();
+        hCleanup.add("value");
+        hCleanup.add("next");
+
+        cleanup(testSubject, hCleanup, new HashSet<String>());
     }
 
     /**
      * - если в списках fieldsToCleanup или fieldsToOutput содержится поле, которого нет в объекте, падать с IllegalArgumentException, оставив объект неизменным
-     * TODO - изменять значения в полях по списку fieldsToCleanup
-     * TODO  - изменять значения полей примитивных типов на значения по умолчанию
-     * TODO  - изменять на null значения полей других типов
+     * - изменять значения в полях по списку fieldsToCleanup
+     *   - изменять значения полей примитивных типов на значения по умолчанию
+     *   - изменять на null значения полей других типов
      * TODO - выводить значения в полях по списку fieldsToOutput
-     * TODO  - выводить String.valueOf для полей примитивных типов
-     * TODO  - выводить toString для полей других типов
-     * TODO - если объект является реализацией интерфейса Map, то проделать аналогичные операции - для списка fieldsToCleanup удалить ключи из мапы, для fieldsToOutput вывести в консоль значения, хранящиеся в мапе.
+     * TODO   - выводить String.valueOf для полей примитивных типов
+     * TODO   - выводить toString для полей других типов
+     * TODO - если объект является реализацией интерфейса Map, то проделать аналогичные операции
+     * TODO   - для списка fieldsToCleanup удалить ключи из мапы
+     * TODO   - для fieldsToOutput вывести в консоль значения, хранящиеся в мапе
      */
     static void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) throws IllegalArgumentException {
         checkIfObjectHasAllFields(object, fieldsToCleanup);
         checkIfObjectHasAllFields(object, fieldsToOutput);
+
+        try {
+            cleanupObject(object, fieldsToCleanup);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Hello, World!");
     }
@@ -34,6 +71,35 @@ public class App {
                 clazz.getDeclaredField(potentialFieldName);
         } catch (NoSuchFieldException e) {
             throw new IllegalArgumentException(e.toString());
+        }
+    }
+
+    private static void cleanupObject(Object object, Set<String> fieldToClean) throws IllegalAccessException {
+        Class clazz = object.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+
+            if (fieldToClean.contains(field.getName())) {
+                if (Modifier.isPrivate(field.getModifiers()))
+                    field.setAccessible(true);
+
+                cleanupField(object, field);
+            }
+        }
+    }
+
+    private static void cleanupField(Object object, Field field) throws IllegalAccessException {
+        switch (field.getType().getName()) { // im deeply unsatisfied with this solution, but it works and that's all that matters
+            case "boolean": field.setBoolean(object, false); break;
+            case "char": field.setChar(object, '\u0000'); break;
+            case "byte": field.setByte(object, (byte)0); break;
+            case "short": field.setShort(object, (short)0); break;
+            case "int": field.setInt(object, 0); System.out.println("int"); break;
+            case "long": field.setLong(object, 0L); break;
+            case "float": field.setFloat(object, 0f); break;
+            case "double": field.setDouble(object, 0d); break;
+            default: field.set(object, null); break;
         }
     }
 }
--- src/test/java/stc/AppTest.java
+++ src/test/java/stc/AppTest.java
@@ -1,16 +1,16 @@
 package stc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.junit.Assert.*;
 
 public class AppTest 
 {
@@ -40,6 +40,86 @@ public class AppTest
         Set<String> hOutput = new HashSet<>();
         hOutput.add("height");
         App.cleanup("String, for example", new HashSet<String>(), hOutput);
+    }
+
+    @Test
+    public void testCleanup() {
+        class Node {
+            private String key;
+            private int value;
+            private Node next;
+
+            private Node(String key, int value, Node next) {
+                this.key = key;
+                this.value = value;
+                this.next = next;
+            }
+
+            private String getKey() {
+                return this.key;
+            }
+
+            private int getValue() {
+                return this.value;
+            }
+        }
+
+        String nodeKey = "some_key";
+        int nodeValue = 42;
+        Node nextNode = new Node("irrelevant_key", 427, null);
+        Node testSubject = new Node(nodeKey, nodeValue, nextNode);
+        assertEquals(nodeKey, testSubject.getKey());
+        assertEquals(nodeValue, testSubject.getValue());
+        assertEquals(nextNode, testSubject.next);
+
+        Set<String> hCleanup = new HashSet<>();
+        hCleanup.add("value");
+        hCleanup.add("next");
+        App.cleanup(testSubject, hCleanup, new HashSet<String>());
+        assertEquals(nodeKey, testSubject.getKey());
+        assertEquals(0, testSubject.getValue());
+        assertNull(testSubject.next);
+    }
+
+    @Test
+    public void testCleanupForAllTypes() {
+        class someClass {
+            private boolean booleanField = true;
+            private char charField = '?';
+            private byte byteField = (byte)231;
+            private short shortField = (short)4416;
+            private int intField = 6112019;
+            private long longField = 3L;
+            private float floatField = 1.1f;
+            private double doubleField = 1.2d;
+            private String stringField = "as good as any other non-primitive type";
+        }
+
+        someClass someObject = new someClass();
+
+        HashSet<String> hCleanup = new HashSet<String>() {{
+            add("booleanField");
+            add("charField");
+            add("byteField");
+            add("shortField");
+            add("intField");
+            add("longField");
+            add("floatField");
+            add("doubleField");
+            add("stringField");
+        }};
+
+        App.cleanup(someObject, hCleanup, new HashSet<String>());
+
+        assertFalse(someObject.booleanField);
+        assertEquals('\u0000', someObject.charField);
+        assertEquals((byte)0, someObject.byteField);
+        assertEquals((short)0, someObject.shortField);
+        assertEquals(0, someObject.intField);
+        assertEquals(0L, someObject.longField);
+        assertEquals(0f, someObject.floatField, 0.0001f);
+        assertEquals(0d, someObject.doubleField, 0.0001d);
+        assertNull(someObject.stringField);
     }
 
     @After
